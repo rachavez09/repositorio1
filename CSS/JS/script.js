cd176930de83f6b62b2f4b7e934d9dec71e85aa7
Ramiro Chávez <ramirochavez@Ramiros-MacBook-Pro.local>
2024-09-28 11:30:15 -0500

    Agregando archivos al repositorio

---
+++ CSS/JS/script.js
@@ -0,0 +1,22 @@
+
+alert("Conectado con éxito")
+
+let nombre = 'Ramiro'
+console.log(nombre);
+let datoingresado=prompt("Ingrese un número");
+console.log(datoingresado);
+
+
+let saludo="Hola Calatita Rules"
+saludo = "Besito para ti Nenita"
+
+console.log(saludo)
+
+let contador=0;
+console.log(contador)
+contador=contador+1
+
+const dni=19191919
+
+dni=0
+
